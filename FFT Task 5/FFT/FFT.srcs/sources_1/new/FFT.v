9e336972b4515386039a41b292911764470ff025
Memory <59786843+Adomnr@users.noreply.github.com>
2024-05-17 10:10:06 +0500

    Add files via upload

---
+++ FFT Task 5/FFT/FFT.srcs/sources_1/new/FFT.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/17/2024 09:46:18 AM
+// Design Name: 
+// Module Name: FFT
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FFT(
+
+    );
+endmodule
