6fdd86cd1c1b83f91e7b7bde0e7b46f95c400227
Memory <lalaubaid.rehman12345@gmail.com>
2025-03-06 22:05:16 +0500

    Adding Bram

---
+++ FreeLancing Projects Files/AudioProcessing/Vivado_Files/AudioProcessing/AudioProcessing.srcs/sources_1/new/bram_controller.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/06/2025 10:05:06 PM
+// Design Name: 
+// Module Name: bram_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module bram_controller(
+
+    );
+endmodule
